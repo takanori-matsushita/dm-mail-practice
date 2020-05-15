176b635a9e070908e3402a17fc15bb9cc769142e
takanori-matsushita <t.matsushita@arcjobz.com>
2020-05-15 15:02:18 +0900

    [add]users-devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  devise_for :admins,  controllers: {
+    sessions: 'admins/sessions'
+  }
 end

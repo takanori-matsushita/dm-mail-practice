36f8fffd7520ccdb74422b0164745aa5701d43b0
takanori-matsushita <t.matsushita@arcjobz.com>
2020-05-18 20:03:40 +0900

    [add]DMテンプレート追加

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,13 +1,31 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'dm_system@mail.com'
-  
-  def send_dm(addresses, mail)
-    @addresses = addresses
+  layout 'mailer'
+
+  def send_dm(user, mail)
+    @user = user
     @mail = mail
       mail(
-        to: @addresses,
+        to: @user.email,
         subject: @mail.title,
-        body: @mail.content
       )
+  end
+  
+  # def send_dm(addresses, mail)
+  #   @addresses = addresses
+  #   @mail = mail
+  #     mail(
+  #       to: @addresses,
+  #       subject: @mail.title,
+  #       body: @mail.content
+  #     )
+  # end
+
+  def send_admin(admin, direct_mail)
+    mail(
+      to: @user.email,
+      subject: @mail.title,
+      body: "#{direct_mail.title} のDMを一斉送信しました"
+    )
   end
 end
